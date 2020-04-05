70dd76da235ba4960b4bf1d84e87695db8386ed7
Paul Lynn <lynnporu@gmail.com>
2020-04-06 02:18:56 +0300

    Add POST playlist method

---
+++ api/v1/playlist/POST-index.php
@@ -0,0 +1,28 @@
+<?php
+
+//unsafe script, no checks here
+
+auth();
+
+$name = demand('name');
+checkGrabErrors();
+
+$ext = [
+	'image/jpeg' => 'jpg',
+	'image/gif' => 'gif',
+	'image/png' => 'png'
+][$_FILES['photo']['type'] ?? 'image/jpeg'];
+
+$photoname = $_SERVER["DOCUMENT_ROOT"] . '/imgs/' . uniqid() . '.' . $ext;
+
+move_uploaded_file($_FILES['photo']['tmp_name'], $photoname);
+
+$GLOBALS['e_mongo']->data->playlists->insertOne([
+	'user_id' => $GLOBALS['auth_user_id'],
+	'photo_url' => $photoname,
+	'name' => $name
+]);
+
+no_content("New playlist was created.");
+
+?>
