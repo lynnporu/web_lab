e07a3b0173c2805f9e7dd3cc661d925f69138307
Paul Lynn <lynnporu@gmail.com>
2020-04-06 03:08:44 +0300

    Add tracks uploading

---
+++ api/v1/music/POST-index.php
@@ -0,0 +1,30 @@
+<?php
+
+//unsafe script, no checks here
+
+auth();
+
+$name = demand('name');
+$author = demand('author');
+checkGrabErrors();
+
+$ext = [
+	'audio/mpeg' => 'mp3',
+	'application/ogg' => 'ogg',
+	'audio/x-wav' => 'wav'
+][$_FILES['photo']['type'] ?? 'audio/mpeg'];
+
+$audioname = $_SERVER["DOCUMENT_ROOT"] . '/audio/' . uniqid() . '.' . $ext;
+
+move_uploaded_file($_FILES['file']['tmp_name'], $audioname);
+
+$GLOBALS['e_mongo']->data->music->insertOne([
+	'user_id' => $GLOBALS['auth_user_id'],
+	'file_url' => $audioname,
+	'name' => $name,
+	'author' => $author
+]);
+
+no_content("New song was created.");
+
+?>
---
+++ js/track_upload.js
@@ -0,0 +1,33 @@
+import { CookieJar } from './classes/CookieJar.js';
+import Loader from './classes/Loader.js';
+import { User } from './classes/User.js';
+
+let done_button = document.getElementById('upload-track-done-button');
+let author_input = document.getElementById('upload-track-author-input');
+let name_input = document.getElementById('upload-track-name-input');
+let file_input = document.getElementById('upload-track-file-input');
+//User.byLogin('me', '123');
+
+let user = User.loadByCookies();
+if (!user) console.error("User data was not loaded.");
+
+file_input.addEventListener('change', e => {
+	done_button.classList.toggle('disabled', false);
+});
+
+done_button.addEventListener('click', e => {
+
+	let form = new FormData();
+	form.append("file", file_input.files[0]);
+
+	Loader.call('post', 'music', {
+		'token': user.access_token,
+		'name': name_input.value,
+		'author': author_input.value
+	}, form)
+		.then(
+			resolve => console.log(resolve),
+			reject => console.error(reject)
+		);
+
+});
