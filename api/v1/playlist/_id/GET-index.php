c7acb76eb05c19b9a683d3635b320eb9b8470b53
Paul Lynn <lynnporu@gmail.com>
2020-05-29 00:35:09 +0300

    Add GET playlist/{id} method

---
+++ api/v1/playlist/_id/GET-index.php
@@ -0,0 +1,63 @@
+<?php
+
+//still no checks, but who cares whoaaa i feel so good right now
+
+$id = demand('id');
+
+auth();
+
+$playlist = $GLOBALS['e_mongo']->data->playlists->find([
+	"_id" => new MongoDB\BSON\ObjectId($id)
+]);
+
+$tracks_cursor = $GLOBALS['e_mongo']->data->playlists->aggregate([
+	[
+		'$match' => ["_id" => new MongoDB\BSON\ObjectId($id)]
+	],
+	[
+		'$unwind' => '$tracks'
+	],
+	[
+		'$lookup' => [
+			'from' => 'music',
+			'localField' => 'tracks',
+			'foreignField' => '_id',
+			'as' => 'track'
+		]
+	],
+	[
+		'$unwind' => '$track'
+	],
+	[
+		'$replaceRoot' => ['newRoot' => '$track']
+	]
+]);
+
+
+$tracks = [];
+
+foreach ($tracks_cursor as $doc) {
+	$tracks[] = [
+		'id'		=> $doc->_id->__toString(),
+		'name'		=> $doc->name,
+		'author'	=> $doc->author,
+		'duration'	=> $doc->duration,
+		'file_url'	=> 'http://localhost:8080/' . ($doc->file_url)
+	];
+}
+
+$playlists_cursor = $GLOBALS['e_mongo']->data->playlists->find(
+	$source == 'profile'
+		? ['user_id' => $GLOBALS['auth_user_id']]
+		: [],
+	["limit" => (int)grab('limit', null, 0)]
+);
+
+emit([
+	'id'		=> $doc->_id->__toString(),
+	'name'		=> $doc->name,
+	'image_url'	=> 'http://localhost:8080/' . ($doc->photo_url),
+	'tracks'	=> $tracks
+]);
+
+?>
